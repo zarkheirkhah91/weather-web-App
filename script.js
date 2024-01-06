1359bcbdb6eaa6fee34eaf3939c5329b9b9d8a75
zar kheir <zahra.kheirkhah91@gmail.com>
2024-01-06 16:50:53 +0000

    commit fetch

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const apiKey = "59bce4e934cea22107fc700406cf42e2";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+// const apiKey = "59bce4e934cea22107fc700406cf42e2";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    const data = await response.json();
+// async function checkWeather(){
+//     const response = await fetch(apiUrl + `&appid=${apiKey}`);
+//     const data = await response.json();
 
-    console.log(data);
-    console.log(response);
+//     console.log(data);
+//     console.log(response);
+// }
+
+
+// checkWeather();
+
+
+
+const weather = {
+    apikey : "59bce4e934cea22107fc700406cf42e2",
+    fetchWeather: function(){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city
+             + "&units=metric&appid="
+             + apikey
+        ).then((Response) => Response.json())
+        .then((data) => console.log(data));
+    },
 }
-
-
-checkWeather();
